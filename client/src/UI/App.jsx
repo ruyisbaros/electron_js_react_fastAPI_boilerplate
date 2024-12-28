d25a1e4b10fccaa4cc5dedb80ccaa96df6239e29
ruyisbaros <ahmet.erdonmez77@gmail.com>
2024-12-28 21:58:54 +0100

    initial till node server

---
+++ client/src/UI/App.jsx
@@ -0,0 +1,5 @@
+const App = () => {
+  return <div className="text-3xl">Hello world</div>;
+};
+
+export default App;
---
+++ client/src/electron/main.js
@@ -0,0 +1,40 @@
+import { app, BrowserWindow } from "electron";
+import path from "path";
+import url from "url";
+import process from "process";
+
+const isDev = process.env.NODE_ENV === "development";
+
+function createWindow() {
+  const mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+      webSecurity: true, // Enabled web security
+      allowFileAccess: true, // Allow file access
+    },
+  });
+  if (isDev) {
+    mainWindow.loadURL("http://localhost:5123");
+  } else {
+    const distPath = path.join(app.getAppPath(), "/dist-react/index.html");
+    const fileUrl = url.pathToFileURL(distPath).toString();
+
+    console.log(`Loading file: ${fileUrl}`);
+    mainWindow.loadURL(fileUrl);
+  }
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on("activate", function () {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+app.on("window-all-closed", function () {
+  if (process.platform !== "darwin") app.quit();
+});
