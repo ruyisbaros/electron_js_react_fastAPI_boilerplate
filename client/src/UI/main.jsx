c4038a9bd54bdc0f8821ad0895319f6ce3437899
ruyisbaros <ahmet.erdonmez77@gmail.com>
2024-12-29 17:07:41 +0100

    CSP set

--- client/src/UI/main.jsx
+++ client/src/UI/main.jsx
@@ -1,10 +1,27 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import process from "process";
+import "./index.css";
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
+const setDynamicCSP = () => {
+  const isDevelopment = process.env.NODE_ENV === "development";
+
+  const csp = isDevelopment
+    ? "default-src 'self'; connect-src 'self' http://localhost:8000; style-src 'self' 'unsafe-inline'; script-src 'self'; img-src 'self' data:; font-src 'self';"
+    : "default-src 'self'; connect-src 'self' https://deployed_domain.com; style-src 'self'; script-src 'self'; img-src 'self' data:; font-src 'self';";
+
+  const metaCSP = document.createElement("meta");
+  metaCSP.httpEquiv = "Content-Security-Policy";
+  metaCSP.content = csp;
+  document.head.appendChild(metaCSP);
+};
+
+// Call the function to set the CSP
+setDynamicCSP();
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
